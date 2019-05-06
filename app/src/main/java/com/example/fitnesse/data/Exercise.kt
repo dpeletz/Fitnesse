76258b05e4aeebbc420a3283593a4013970e12f8
dpeletz <dpeletz@macalester.edu>
2019-05-06 11:34:07 +0200

    update Exercise Data Class

--- app/src/main/java/com/example/fitnesse/data/Exercise.kt
+++ app/src/main/java/com/example/fitnesse/data/Exercise.kt
@@ -1,7 +1,11 @@
 package com.example.fitnesse.data
 
 data class Exercise (
+    var exerciseID: String,
+    var userID: String,
     var name : String,
     var time : Int,
+    var bodyweight: Boolean,
+    var bodyPartCategory: Int,
     var reps : Int
 )
