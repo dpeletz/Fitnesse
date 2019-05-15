329d7e5fc501bfd4e37eff1538426368de40eb52
carokun <carokun@sas.upenn.edu>
2019-05-15 11:35:07 +0200

    updated UI files (profile)

--- app/src/main/java/com/example/fitnesse/ProfileActivity.kt
+++ app/src/main/java/com/example/fitnesse/ProfileActivity.kt
@@ -2,11 +2,29 @@ package com.example.fitnesse
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val editTexts = listOf<TextInputEditText>(
+            etName,
+            etGender,
+            etAge,
+            etHeight,
+            etWeight,
+            etBMI
+        )
+
+        for (editText in editTexts) {
+            editText.isEnabled = false
+            editText.keyListener = null
+            editText.setTextIsSelectable(false)
+        }
     }
 }
